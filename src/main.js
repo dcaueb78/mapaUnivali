72e72985bb753d2d50ca584913e8984f827cd08d
Cauê Kotarski <caue1017@hotmail.com>
2019-05-09 17:26:58 -0300

    Localização usuário

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-
-function darkness(){
+function darkness() {
     let map = document.getElementById("map");
-    if(map.classList == "darkmap"){
+    if (map.classList == "darkmap") {
         map.classList = "whitemap";
-    }else{
+    } else {
         map.classList = "darkmap";
     }
 }
@@ -15,3 +14,14 @@ var marker = new mapboxgl.Marker(elemento)
     .setLngLat([-48.665681, -26.917887])
     .addTo(map)
 
+// var nav = new mapboxgl.NavigationControl();
+// map.addControl(nav, 'top-left');
+
+// Add geolocate control to the map.
+
+map.addControl(new mapboxgl.GeolocateControl({
+positionOptions: {
+enableHighAccuracy: true
+},
+trackUserLocation: true
+}));
